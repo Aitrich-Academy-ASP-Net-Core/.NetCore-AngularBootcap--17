5a60c395a1ed518a70855d69f5533e13f94af2ff
nehacj2003 <nehacjjayakrishnan2003@gmail.com>
2025-04-07 13:22:27 +0530

    added

---
+++ .netCore/OOPs/oops exam/neha machinetest/LibraryMember.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace neha_machinetest.Libarymember
+{
+    public abstract class LibraryMember
+    {
+        public int Memberid { get; set; }
+        public string Name { get; set; }
+        
+           
+        public LibraryMember(int memberid,string name)
+        {
+            Memberid = memberid;
+            Name = name;
+           
+            
+        }
+        public abstract double CalculateFine(int overdueDays);
+
+    }
+   
+}
