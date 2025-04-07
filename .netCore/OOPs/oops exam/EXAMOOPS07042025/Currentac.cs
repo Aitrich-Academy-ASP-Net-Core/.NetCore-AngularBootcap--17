69be3e193188b6f498809970a32235be9609aab0
sageer32 <me.sageermohammed@gmail.com>
2025-04-07 12:28:23 +0530

    oop exam added

---
+++ .netCore/OOPs/oops exam/EXAMOOPS07042025/Currentac.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAMOOPS07042025
+{
+    public class Currentac : Savingsac
+    {
+        public void Applymainfee(int mainfee)
+        {
+            Console.WriteLine("CURRENT ACCOUNT HOLDER NAME: " + Acholder);
+            Console.WriteLine("MAINTENANCE FEE: " + mainfee);
+            Console.WriteLine($"CURRENT ACCOUNT BALANCE: {Balance - mainfee}");
+        }
+    }
+}
