44054cb96b7a11091488b61d9ed26c2729e291d0
sageer32 <me.sageermohammed@gmail.com>
2025-04-07 11:35:50 +0530

    C SHARP OOPS EXAM ADDED

---
+++ .netCore/OOPs/oops exam/EXAMOOPS07042025/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EXAMOOPS07042025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            Savingsac sac = new Savingsac();
+            sac.Acholder = "SARA";
+            sac.Balance = 30000;
+            sac.Calcinterest();
+
+
+            Currentac cac = new Currentac();
+            cac.Acholder = "ALEN";
+            cac.Balance = 40000;
+            cac.Applymainfee(500);
+
+        }
+    }
+}
