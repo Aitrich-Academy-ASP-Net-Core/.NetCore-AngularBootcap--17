d4f76a14a81ab52f2e7d71e1ed92a62f007f99d2
shemeera-k-muhammed <shemirazak67@gmail.com>
2025-04-21 14:52:19 +0530

    activity

---
+++ .netCore/OOPs/Students/Shemeera/New folder/INHERITACE PROBLEMS/InheritanceEmployee/InheritanceEmployee/Program.cs
@@ -0,0 +1,12 @@
+﻿using InheritanceEmployee;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Manager manager=new Manager("beena", 2000,1000);
+        manager.Display();
+        manager.TotalSalary();
+
+    }
+}
---
+++ .netCore/OOPs/Students/Shemeera/New folder/INHERITACE PROBLEMS/InheritanceVehicle/InheritanceVehicle/Program.cs
@@ -0,0 +1,10 @@
+﻿using InheritanceVehicle;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Car car = new Car(60,"bmw","aaa",1970);
+        car.DisplayDetails();
+    }
+}
