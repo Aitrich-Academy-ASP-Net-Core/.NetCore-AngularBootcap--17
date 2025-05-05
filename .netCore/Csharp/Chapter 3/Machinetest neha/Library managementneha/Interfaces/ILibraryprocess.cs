7cdfa238b4c16b048f9dc3ea3440c50128824013
nehacj2003 <nehacjjayakrishnan2003@gmail.com>
2025-05-05 21:32:20 +0530

    neha exam

---
+++ .netCore/Csharp/Chapter 3/Machinetest neha/Library managementneha/Interfaces/ILibraryprocess.cs
@@ -0,0 +1,20 @@
+﻿using Library_managementneha.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_managementneha.Interfaces
+{
+   public interface ILibraryprocess
+    {
+        public void DisplayAdd(Book book);
+        public void DisplayRemove(string isbn);
+        public void DisplayBorrow(string isbn);
+        public void DisplayBook();
+
+
+
+    }
+}
