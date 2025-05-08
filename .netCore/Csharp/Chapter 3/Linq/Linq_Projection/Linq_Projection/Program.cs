916981a9ad2bdb0445efaf276fddb21a4ff8f013
nehacj2003 <nehacjjayakrishnan2003@gmail.com>
2025-05-08 12:25:13 +0530

    added

---
+++ .netCore/Csharp/Chapter 3/Linq/Linq_Projection/Linq_Projection/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Linq_Projection
+{
+	class Program
+	{
+		public static void Main()
+		{
+			var words = new string[] { "apple", "orange", "mango" };
+
+			var query =from word in words select word.Substring(0,1);
+
+			foreach (var word in query) {
+			
+			Console.WriteLine(word);
+			}
+			//a,o,m
+
+			var sentences = new string[] { "Aitrich technologies ", "the quick brown fox" };
+			var querys = from sentence in sentences
+						 from word in sentence.Split(' ')
+						 select word;
+
+			foreach (var sentence in querys) {
+			Console.WriteLine(querys);
+			}
+			var list1 = new int[] {1,2,3,4};
+			var list2 = new string[] { "a", "b", "c"};
+			var quer = Enumerable.Zip(list1, list2,(num,letter)=>num.ToString()+letter);
+
+			foreach(var item in query)
+			{
+				Console.WriteLine(item);
+			}
+
+		}
+	}
+}
---
+++ .netCore/Csharp/Chapter 3/Linq/SetOperations/SetOperations/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace SetOperations
+{
+	internal class Program
+	{
+		public static void Main(string[] args) {
+			int[] list1 = { 1, 2, 3,5};
+			int[] list2 = { 4,5, 6 };
+			var numbers = from num in list1.Union(list2) select num;
+
+			foreach (int num in numbers)
+			{
+				Console.WriteLine(num);
+			}
+
+			//intersection
+			Console.WriteLine(" intersect ");
+			var number = from num in list1.Intersect(list2) select num;
+
+			foreach (int num in number)
+			{
+				Console.WriteLine(num);
+			}
+			//distinct
+			Console.WriteLine("distinct");
+			int[] list3 = { 1, 2, 1, 3, 1 };
+			var distinct = from num in list3.Distinct() select num;
+			foreach(int num in distinct)
+			{
+				Console.WriteLine(num);
+			}
+			//Except
+			Console.WriteLine("Except");
+			var except =
+			from num in list1.Except(list3) select num;
+			foreach (int num in except)
+			{
+				Console.WriteLine(num);
+			}
+
+		}
+	}
+}
