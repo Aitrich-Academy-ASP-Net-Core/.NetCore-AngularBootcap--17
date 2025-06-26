595a5adc8df2542e4654b2bfc6fce3c2d2ccc03c
shemeera-k-muhammed <shemirazak67@gmail.com>
2025-06-26 21:30:22 +0530

    added

--- .netCore/EFCore/Activity/EF-CodeFirstWithFK/EF-CodeFirstWithFK/obj/Debug/net8.0/EF-CodeFirstWithFK.AssemblyInfo.cs
+++ .netCore/EFCore/Activity/EF-CodeFirstWithFK/EF-CodeFirstWithFK/obj/Debug/net8.0/EF-CodeFirstWithFK.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EF-CodeFirstWithFK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed2d5a3219b06d67360988f0bcc4de23e40c83bd")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+623c793e15f26b768093bab84d023cb02fc607ef")]
->>>>>>> b53938cb2e245e2c297742f3c51cb7c9f69adb60
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f1939bc0cc19b49e5341f8860073a2736875514")]
 [assembly: System.Reflection.AssemblyProductAttribute("EF-CodeFirstWithFK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EF-CodeFirstWithFK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
