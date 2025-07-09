11eb1c75880622ee8c6e6a668f4edf7d5d45211b
sageer32 <me.sageermohammed@gmail.com>
2025-07-09 15:14:59 +0530

    SAGEER RAZOR EXAM ADDED

---
+++ .netCore/Razor/Sageer exam/RazorExamm/Extension/ApplicationServiceExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorExamm.Models;
+using RazorExamm.Service;
+
+namespace RazorExamm.Extension
+{
+    public class ApplicationServiceExtension
+    {
+        public static IServiceCollection AddApplicationServices
+             (this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<BookDBContext>(options =>
+            options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+            services.AddScoped<BookServices>();
+            services.AddScoped<BookRepo>();
+
+            
+
+
+            return services;
+        }
+    }
+}
