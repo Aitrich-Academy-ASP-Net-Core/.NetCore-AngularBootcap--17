74de6d1d1c5f007c638cee46c114f345bbe29063
reeshma <reeshma.aitrich@gmail.com>
2025-07-11 17:05:15 +0530

    added

--- .netCore/Razor/Husna/Exam/LibraryManagement/obj/Debug/net8.0/LibraryManagement.AssemblyInfo.cs
+++ .netCore/Razor/Husna/Exam/LibraryManagement/obj/Debug/net8.0/LibraryManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d9d4b14c7af4828a7fe0ba0a9fa66034ece019a")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
