8b276a116adec2c1e83ef1ce5d74aa26c8467390
reeshma <reeshma.aitrich@gmail.com>
2025-08-13 12:30:30 +0530

    workshop added

--- .netCore/MVC/Activity/JobPortal-Activity/JobPortal-Activity/obj/Debug/net8.0/JobPortal-Activity.AssemblyInfo.cs
+++ .netCore/MVC/Activity/JobPortal-Activity/JobPortal-Activity/obj/Debug/net8.0/JobPortal-Activity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobPortal-Activity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8ec3eef955c7ed5f252b3295ca28932ca410eac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a74fadd260f89da338b57754c2f789b086f7863")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobPortal-Activity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobPortal-Activity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
