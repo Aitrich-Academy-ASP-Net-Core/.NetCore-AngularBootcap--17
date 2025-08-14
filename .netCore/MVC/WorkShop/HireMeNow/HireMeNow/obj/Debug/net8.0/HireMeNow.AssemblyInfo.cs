70f4e3da8ffc231b9742f224d08f51fc71f70b60
reeshma <reeshma.aitrich@gmail.com>
2025-08-14 11:04:50 +0530

    del

--- .netCore/MVC/WorkShop/HireMeNow/HireMeNow/obj/Debug/net8.0/HireMeNow.AssemblyInfo.cs
+++ .netCore/MVC/WorkShop/HireMeNow/HireMeNow/obj/Debug/net8.0/HireMeNow.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HireMeNow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65582ee78a53844cc089886a060cd139fac52cf4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98ecc066df06b8470aad92a39d91a09dc95f93e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HireMeNow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HireMeNow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
