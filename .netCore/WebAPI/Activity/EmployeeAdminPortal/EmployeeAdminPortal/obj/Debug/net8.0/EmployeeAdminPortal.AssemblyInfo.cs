b519610d627011a3771d39dfdc287bedc707934b
reeshma <reeshma.aitrich@gmail.com>
2025-09-09 12:34:25 +0530

    added

--- .netCore/WebAPI/Activity/EmployeeAdminPortal/EmployeeAdminPortal/obj/Debug/net8.0/EmployeeAdminPortal.AssemblyInfo.cs
+++ .netCore/WebAPI/Activity/EmployeeAdminPortal/EmployeeAdminPortal/obj/Debug/net8.0/EmployeeAdminPortal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e41e2b2856e7d0fdb905ed955313c9e0e79d96a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01d2a4ff3f51f4d988018513e311e1d29a0f6070")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- .netCore/WebAPI/Activity/SendMail/SendMail/obj/Debug/net8.0/SendMail.AssemblyInfo.cs
+++ .netCore/WebAPI/Activity/SendMail/SendMail/obj/Debug/net8.0/SendMail.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SendMail")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e41e2b2856e7d0fdb905ed955313c9e0e79d96a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01d2a4ff3f51f4d988018513e311e1d29a0f6070")]
 [assembly: System.Reflection.AssemblyProductAttribute("SendMail")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SendMail")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
