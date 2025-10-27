816b5a26052c2f94a432eea8861c091fb2a3b6a8
husnajabir <husnajabir52@gmail.com>
2025-10-27 14:32:21 -0700

    deleting

--- .netCore/WebAPI/PROJECT2/Job/SavedJobObjects/SaveJobRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JobPortalApp.API.Job.SavedJobObjects
-{
-	public class SaveJobRequest
-	{
-		public Guid SavedBy { get; set; }
-		public Guid Job { get; set; }
-		public DateTime DateSaved { get; set; }
-	}
-}
