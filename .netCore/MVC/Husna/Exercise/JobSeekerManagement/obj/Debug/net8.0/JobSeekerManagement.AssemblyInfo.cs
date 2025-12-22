5e6697abe9b8b9b8f9737d1d556b256c1d2b68a6
reeshma <reeshma.aitrich@gmail.com>
2025-12-22 10:18:22 +0530

    added

--- .netCore/MVC/Husna/Exercise/JobSeekerManagement/obj/Debug/net8.0/JobSeekerManagement.AssemblyInfo.cs
+++ .netCore/MVC/Husna/Exercise/JobSeekerManagement/obj/Debug/net8.0/JobSeekerManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobSeekerManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+688772d460b971c091434eff53a2a47dbef5513e")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobSeekerManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobSeekerManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
